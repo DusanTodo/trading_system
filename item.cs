2c89f64bc93c622669250c953560a586faeb3c30
github username <dusan.todorovic@student.nbi-handelsakademin.se>
2025-09-30 14:22:19 +0200

    Created Item.cs class

--- item.cs
+++ item.cs
@@ -2,13 +2,11 @@ namespace App;
 {
     public class Item
     {
-        // Enkel autoincrement (observera: nollställs när programmet startar om; vi löser detta när vi implementerar persistens)
         private static int _nextId = 1;
 
         public int Id { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        // Spara ägaren som användarnamn för att undvika komplex serialisering av User-objekt i första steget
         public string Owner { get; private set; }
 
         public Item(string name, string description, string owner)
