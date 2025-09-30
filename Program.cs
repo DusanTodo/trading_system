c252d161cdf023f34763b20a35f584d968319da9
github username <dusan.todorovic@student.nbi-handelsakademin.se>
2025-09-30 13:25:59 +0200

    Things done: Welcome menu and switch beggining

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using App;
 
-List<User> users = new list<User>();
+List<User> users = new List<User>();
 
 users.Add(new User("Dusan", "d@t", "pass"));
 
@@ -12,14 +12,93 @@ while (running)
 {
     Console.Clear();
 
-    Console.WriteLine("Welcome To Trading System In Terminal");
-    Console.WriteLine("");
 
     if (active_user == null)
     {
-        Console.Clear()
-        Console.WriteLine("1. log in");
-        Console.WriteLine("2. register");
-        switch(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("Welcome To Trading System In Terminal");
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("1. Log in");
+        Console.WriteLine("2. Register");
+        Console.WriteLine("3. Exit");
+        Console.Write("Choose: ");
+        string? choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Console.Clear();
+                System.Console.Write("Username: ");
+                string username = Console.ReadLine();
+                System.Console.Write("Email: ");
+                string email = Console.ReadLine();
+                System.Console.Write("Password: ");
+                string password = Console.ReadLine();
+                foreach (User user in users)
+                {
+                    if (username == user.Username && password == user.Password)
+                    {
+                        Console.Clear();
+                        active_user = user;
+                        System.Console.WriteLine($"Welcome {username}!");
+                        Console.ReadLine();
+                        break;
+                    }
+                }
+                if (active_user == null)
+                {
+                    Console.WriteLine("Incorrect username or password, please try again.");
+                    Console.ReadLine();
+                }
+
+                break;
+
+            case "2":
+                Console.Clear();
+                System.Console.Write("Username: ");
+                username = Console.ReadLine();
+                System.Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Clear();
+                System.Console.Write("Password: ");
+                password = Console.ReadLine();
+                users.Add(new User(username, email, password));
+                Console.Clear();
+                System.Console.WriteLine($"{username} created.");
+                Console.ReadLine();
+                break;
+
+            case "3":
+
+                running = false;
+                break;
+
+
+        }
+
     }
+
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"Logged in as {active_user.Username} ({active_user.Email})");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("1. Logout");
+        Console.WriteLine("2. Upload item");
+        Console.WriteLine("3. Browse items");
+        Console.WriteLine("4. Browse trade requests");
+        Console.WriteLine("5. Delete account");
+        Console.WriteLine("5. Exit");
+        Console.Write("Choose: ");
+        string? choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                active_user = null;
+                break;
+            
+            
+        }
+    }
+    
 }
--- user.cs
+++ user.cs
@@ -7,18 +7,18 @@ class User
 
     public string Email;
 
-    string _password
+    public string Password;
 
     public User(string u, string e, string p)
     {
-        Username = u -;
-        Email - e;
-        _password = p;
+        Username = u;
+        Email = e;
+        Password = p;
     }
 
     public bool TryLogin(string username, string password)
     {
-        return username == Email && password == _password;
-        
+        return username == Email && password == Password;
+
     }
 }
